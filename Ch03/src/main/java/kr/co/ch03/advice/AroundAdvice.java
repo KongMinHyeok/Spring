cbb9310db77ffe88eb1d66cf305f5d81ee748854
unknown <dlwlzjffj1@naver.com>
2023-01-04 13:50:13 +0900

    2023/01/04 Spring MVC 실습

--- Ch03/src/main/java/kr/co/ch03/advice/AroundAdvice.java
+++ Ch03/src/main/java/kr/co/ch03/advice/AroundAdvice.java
@@ -1,24 +1,32 @@
 package kr.co.ch03.advice;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AroundAdvice {
 
 	
-	public void around1(ProceedingJoinPoint pjp) {
-		
+	@Pointcut("execution(* kr.co.ch03.AOPService.delete*(..))")
+	public void deletePointcut() {}
+	
+	@Pointcut("execution(* kr.co.ch03.AOPService.insert*(..))")
+	public void insertPointcut() {}
+	
+	@Around("deletePointcut()")
+	public void around1(ProceedingJoinPoint pjp) throws Throwable {
 		System.out.println("횡단관심 - around1 before");
 		pjp.proceed();
 		System.out.println("횡단관심 - around1 after");
 	}
 	
-	public void around1(ProceedingJoinPoint pjp) {
-		
-		System.out.println("횡단관심 - around1 before");
+	@Around("insertPointcut()")
+	public void around2(ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("횡단관심 - around2 before");
 		pjp.proceed();
-		System.out.println("횡단관심 - around1 after");
+		System.out.println("횡단관심 - around2 after");
 	}
 	
 }
